6690d114ec2df9a164e78667b1858a0335b873c6
Mohamed-212 <Mohamed@212solutionsllc.com>
2021-05-09 15:50:07 +0200

    9/5

--- app/Http/Middleware/ChooseService.php
+++ app/Http/Middleware/ChooseService.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace App\Http\Middleware;
@@ -17,35 +16,8 @@ class ChooseService
     public function handle($request, Closure $next)
     {
         if (!session()->has('branch_id')){
-            return redirect()->route('service.page');
+            return redirect('menu')->with('status', 'not');
         }
         return $next($request);
     }
 }
-||||||| 92bdb17
-=======
-<?php
-
-namespace App\Http\Middleware;
-
-use Closure;
-
-class ChooseService
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        if (!session()->has('branch_id')){
-
-            return redirect()->route('menu.page');
-        }
-        return $next($request);
-    }
-}
->>>>>>> abd_2
